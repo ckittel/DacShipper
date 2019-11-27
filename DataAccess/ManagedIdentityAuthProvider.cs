38fcd4890ce105718dcc375ac85ccf59b843ab48
Chad Kittel <chad.kittel@gmail.com>
2019-11-27 11:41:41 -0600

    add comment about singleton

--- DataAccess/ManagedIdentityAuthProvider.cs
+++ DataAccess/ManagedIdentityAuthProvider.cs
@@ -5,6 +5,8 @@ namespace DacShipper
 {
     public class ManagedIdentityAuthProvider : IUniversalAuthProvider
     {
+        // TODO: This assumes that ManagedIdentidyAuthProvider is a singleton.  If it's not, AzureServiceTokenProvider
+        // should be injected as a singleton.
         private readonly AzureServiceTokenProvider _managedIdentityTokenProvider = new AzureServiceTokenProvider();
 
         // TODO: if your identity belongs in multiple directories, you'll need to provide that directory ID in the GetAccessTokenAsync call below.
